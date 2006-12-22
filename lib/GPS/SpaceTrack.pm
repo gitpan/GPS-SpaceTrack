21695e027ad1b9097354ee265ba69be4434ccd2e
Michael R. Davis <mrdvt@cpan.org>
2006-12-22 00:51:50 +0000

    Import of MRDVT/GPS-SpaceTrack-0.02 from CPAN.
    gitpan-cpan-distribution: GPS-SpaceTrack
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MRDVT/GPS-SpaceTrack-0.02.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- lib/GPS/SpaceTrack.pm
+++ lib/GPS/SpaceTrack.pm
@@ -6,8 +6,12 @@ GPS::SpaceTrack - Package for calculating the position of GPS satellites
 
 =head1 SYNOPSIS
 
-  use GPS::SpaceTrack;
-  my $obj = GPS::SpaceTrack->new(filename=>"./gps.tle");
+use GPS::SpaceTrack;
+my $obj=GPS::SpaceTrack->new(filename=>"gps.tle");
+print join("\t", qw{Count PRN ELEV Azim SNR USED}), "\n";
+foreach ($obj->getsatellitelist({lat=>38.870997, lon=>-77.05596})) {
+  print join("\t", $_->prn, $_->elev, $_->azim, $_->snr, $_->used), "\n";
+}
 
 =head1 DESCRIPTION
 
@@ -19,7 +23,7 @@ Function naming convention is "format of the return" underscore "format of the p
 
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%02d", q{Revision: 0.01} =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q{Revision: 0.02} =~ /(\d+)\.(\d+)/);
 use Astro::Coord::ECI;
 use Astro::Coord::ECI::TLE;
 use Net::GPSD::Satellite;
