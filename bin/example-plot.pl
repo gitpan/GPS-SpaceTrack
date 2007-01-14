ab8e6f473b59329899d0a4c276f6550a5d07182e
Michael R. Davis <mrdvt@cpan.org>
2007-01-14 02:03:46 +0000

    Import of MRDVT/GPS-SpaceTrack-0.06 from CPAN.
    gitpan-cpan-distribution: GPS-SpaceTrack
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MRDVT/GPS-SpaceTrack-0.06.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- bin/example-plot.pl
+++ bin/example-plot.pl
@@ -4,6 +4,14 @@
 
 example-plot.pl - Plot GPS::SpaceTrack data with GD::Graph::Polar
 
+=head1 SAMPLE OUTPUT
+
+=begin html
+
+<img src="http://search.cpan.org/src/MRDVT/GPS-SpaceTrack-0.06/bin/example-plot.png" />
+
+=end html 
+
 =cut
 
 use strict;
@@ -20,24 +28,45 @@ my $time=time();               #seconds
 my $filename="";
 $filename="../doc/gps.tle" if -r "../doc/gps.tle";
 $filename="./doc/gps.tle" if -r "./doc/gps.tle";
+$filename="./gps.tle" if -r "./gps.tle";
+$filename="../gps.tle" if -r "../gps.tle";
+$filename="../../gps.tle" if -r "../../gps.tle";
 
 my $plot=GD::Graph::Polar->new(size=>480, radius=>90, ticks=>9);
 my $obj=GPS::SpaceTrack->new(filename=>$filename) || die();
 
 my $count=0;
-foreach my $sec (0..80) {
+my @data=();
+foreach my $sec (0..70) {
   $sec*=400;
   $sec+=$time;
-  foreach (grep {$_->elev > 0} $obj->getsatellitelist({lat=>$lat, lon=>$lon, alt=>$hae, time=>$sec})) {
+  foreach (grep {$_->elev > 0} $obj->getsatellitelist({lat=>$lat,
+                                                       lon=>$lon,
+                                                       alt=>$hae,
+                                                       time=>$sec})) {
     my $r=90-$_->elev;
     my $t=$_->azim;
-    $plot->addGeoPoint($r=>$t);
+    $count++;
+    push @data, [$_->prn, $count, $r=>$t];
     {
       local $|=1;
-      print $count++, "\r";
+      print $count, "\r";
     }
   }
 } 
+my %prn=map {$_->[0] => 1} @data;
+foreach my $prn (keys %prn) {
+  my @list=grep {$_->[0] eq $prn} @data;
+  foreach (1..$#list) {
+    my $r0=$list[$_-1]->[2];
+    my $t0=$list[$_-1]->[3];
+    my $r1=$list[$_]->[2];
+    my $t1=$list[$_]->[3];
+    $plot->addGeoPoint($r0=>$t0) if 1==$_;
+    $plot->addGeoPoint($r1=>$t1) if $#list==$_;
+    $plot->addGeoLine($r0=>$t0, $r1=>$t1);
+  }
+}
 print "\nTime: ", time()-$time, " seconds\n";
 open(IMG, ">example-plot.png");
 print IMG $plot->draw;
